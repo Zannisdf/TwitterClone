ed7ad422537af2148dc4892c9e3715f9d83e938b
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-22 11:21:57 -0300

    ADD: title support

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_134941) do
+ActiveRecord::Schema.define(version: 2019_01_22_142018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_01_22_134941) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
